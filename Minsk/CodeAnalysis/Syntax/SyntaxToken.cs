9497f1bbce8f17bb4da66a673742edd683a37792
Alberto Spelta <alberto.spelta@live.com>
2020-05-11 14:15:19 +0200

    Fix null reference for cases where the token was inserted

--- Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
+++ Minsk/CodeAnalysis/Syntax/SyntaxToken.cs
@@ -19,6 +19,6 @@ namespace Minsk.CodeAnalysis.Syntax
         public string Text { get; }
         public object Value { get; }
 
-        public override TextSpan Span => new TextSpan(Position, Text.Length);
+        public override TextSpan Span => new TextSpan(Position, Text?.Length ?? 0);
     }
 }
