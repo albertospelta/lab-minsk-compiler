afa5167e415bc40c1cae0e865c2508fe2d99711c
Alberto Spelta <alberto.spelta@live.com>
2020-05-12 20:58:04 +0200

    Add compilation unit

--- Minsk/CodeAnalysis/Syntax/AssignmentExpressionSyntax.cs
+++ Minsk/CodeAnalysis/Syntax/AssignmentExpressionSyntax.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace Minsk.CodeAnalysis.Syntax
 {
---
+++ Minsk/CodeAnalysis/Syntax/CompilationUnitSyntax.cs
@@ -0,0 +1,16 @@
+﻿namespace Minsk.CodeAnalysis.Syntax
+{
+    public sealed class CompilationUnitSyntax : SyntaxNode
+    {
+        public CompilationUnitSyntax(ExpressionSyntax expression, SyntaxToken endOfFileToken)
+        {
+            Expression = expression;
+            EndOfFileToken = endOfFileToken;
+        }
+
+        public override SyntaxKind Kind => SyntaxKind.CompilationUnit;
+        public ExpressionSyntax Expression { get; private set; }
+        public SyntaxToken EndOfFileToken { get; private set; }
+
+    }
+}
