c3cc0c2cad27823ea65cad44d2c39257435e1b9b
Alberto Spelta <alberto.spelta@live.com>
2020-05-12 20:31:23 +0200

    Add test coverage for SourceText must includes last line

---
+++ Minsk.Tests/CodeAnalysis/Syntax/SyntaxTextTests.cs
@@ -0,0 +1,18 @@
+﻿using Minsk.CodeAnalysis.Text;
+using Xunit;
+
+namespace Minsk.Test.CodeAnalysis.Syntax
+{
+    public class SyntaxTextTests
+    {
+        [Theory]
+        [InlineData(".", 1)]
+        [InlineData(".\r\n", 2)]
+        [InlineData(".\r\n\r\n", 3)]
+        public void  SourceText_IncludesLastLine(string text, int exprectedLineCount)
+        {
+            var sourceText = SourceText.From(text);
+            Assert.Equal(exprectedLineCount, sourceText.Lines.Length);
+        }
+    }
+}
