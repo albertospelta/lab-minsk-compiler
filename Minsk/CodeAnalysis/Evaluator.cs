1f7e6eafacb54f5abfeda9296405499014553ab9
Alberto Spelta <alberto.spelta@live.com>
2020-05-21 10:40:23 +0200

    Converted switch from expression to statemen

--- Minsk/CodeAnalysis/Evaluator.cs
+++ Minsk/CodeAnalysis/Evaluator.cs
@@ -137,22 +137,35 @@ namespace Minsk.CodeAnalysis
             var left = EvaluateExpression(b.Left);
             var right = EvaluateExpression(b.Right);
 
-            return b.Operator.Kind switch
+            switch (b.Operator.Kind)
             {
-                BoundBinaryOperatorKind.Addition => (int)left + (int)right,
-                BoundBinaryOperatorKind.Subtraction => (int)left - (int)right,
-                BoundBinaryOperatorKind.Multiplication => (int)left * (int)right,
-                BoundBinaryOperatorKind.Division => (int)left / (int)right,
-                BoundBinaryOperatorKind.LogicalAnd => (bool)left && (bool)right,
-                BoundBinaryOperatorKind.LogicalOr => (bool)left || (bool)right,
-                BoundBinaryOperatorKind.Equals => Equals(left, right),
-                BoundBinaryOperatorKind.NotEquals => !Equals(left, right),
-                BoundBinaryOperatorKind.Less=> (int)left < (int)right,
-                BoundBinaryOperatorKind.LessOrEquals => (int)left <= (int)right,
-                BoundBinaryOperatorKind.Greater => (int)left > (int)right,
-                BoundBinaryOperatorKind.GreaterOrEquals => (int)left >= (int)right,
-                _ => throw new Exception($"Unexpected binary operator '{ b.Operator }'")
-            };
+                case BoundBinaryOperatorKind.Addition:
+                    return (int)left + (int)right;
+                case BoundBinaryOperatorKind.Subtraction:
+                    return (int)left - (int)right;
+                case BoundBinaryOperatorKind.Multiplication:
+                    return (int)left * (int)right;
+                case BoundBinaryOperatorKind.Division:
+                    return (int)left / (int)right;
+                case BoundBinaryOperatorKind.LogicalAnd:
+                    return (bool)left && (bool)right;
+                case BoundBinaryOperatorKind.LogicalOr:
+                    return (bool)left || (bool)right;
+                case BoundBinaryOperatorKind.Equals:
+                    return Equals(left, right);
+                case BoundBinaryOperatorKind.NotEquals:
+                    return !Equals(left, right);
+                case BoundBinaryOperatorKind.Less:
+                    return (int)left < (int)right;
+                case BoundBinaryOperatorKind.LessOrEquals:
+                    return (int)left <= (int)right;
+                case BoundBinaryOperatorKind.Greater:
+                    return (int)left > (int)right;
+                case BoundBinaryOperatorKind.GreaterOrEquals:
+                    return (int)left >= (int)right;
+                default:
+                    throw new Exception($"Unexpected binary operator '{ b.Operator }'");
+            }
         }
     }
 }
