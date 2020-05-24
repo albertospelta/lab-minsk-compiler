a79edfe8c89fd432c33e3a12ab6de423403fc694
Alberto Spelta <alberto.spelta@live.com>
2020-05-24 13:24:27 +0200

    Lower if, while, and for into gotos

--- Minsk/CodeAnalysis/Compilation.cs
+++ Minsk/CodeAnalysis/Compilation.cs
@@ -64,7 +64,7 @@ namespace Minsk.CodeAnalysis
             statement.WriteTo(writer);
         }
 
-        private BoundStatement GetStatement()
+        private BoundBlockStatement GetStatement()
         {
             var statement = GlobalScope.Statement;
             return Lowerer.Lower(statement);
