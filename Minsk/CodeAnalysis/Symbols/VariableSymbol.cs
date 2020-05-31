051caa0d8002557419a07fcf7530e193060d9cd1
Alberto Spelta <alberto.spelta@live.com>
2020-05-31 19:07:27 +0200

    Add abstract Symbol type

--- Minsk/CodeAnalysis/Symbols/VariableSymbol.cs
+++ Minsk/CodeAnalysis/Symbols/VariableSymbol.cs
@@ -2,18 +2,19 @@
 
 namespace Minsk.CodeAnalysis.Symbols
 {
-    public sealed class VariableSymbol
+    public sealed class VariableSymbol : Symbol
     {
         public VariableSymbol(string name, bool isReadOnly, Type type)
+            : base(name)
         {
-            Name = name;
             IsReadOnly = isReadOnly;
             Type = type;
         }
 
-        public string Name { get; }
         public bool IsReadOnly { get; }
         public Type Type { get; }
+
+        public override SymbolKind Kind => SymbolKind.Variable;
 
         public override string ToString() => Name;
     }
