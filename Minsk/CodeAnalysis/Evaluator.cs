d67b0cd5744f3b51508404fef37f44a59aeb8fb2
Alberto Spelta <alberto.spelta@live.com>
2020-06-07 11:34:51 +0200

    Add support for string concatenation

--- Minsk/CodeAnalysis/Evaluator.cs
+++ Minsk/CodeAnalysis/Evaluator.cs
@@ -125,7 +125,10 @@ namespace Minsk.CodeAnalysis
             switch (b.Operator.Kind)
             {
                 case BoundBinaryOperatorKind.Addition:
-                    return (int)left + (int)right;
+                    if (b.Type == TypeSymbol.Int)
+                        return (int)left + (int)right;
+                    else
+                        return (string)left + (string)right;
                 case BoundBinaryOperatorKind.Subtraction:
                     return (int)left - (int)right;
                 case BoundBinaryOperatorKind.Multiplication:
